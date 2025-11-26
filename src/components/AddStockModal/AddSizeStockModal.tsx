8b4a315faebc9e86225119b9f875cf1cfc49902d
Golam Rabbani <golamrabbani9373@gmail.com>
2025-11-26 16:03:38 +0600

    update size stock modal

--- src/components/AddStockModal/AddSizeStockModal.tsx
+++ src/components/AddStockModal/AddSizeStockModal.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import React, {useEffect, useState} from 'react';
 import deleteModalStyles from '../DeleteModal/DeleteModal.module.css';
 import ZForm from '../form/ZForm';
 import ZMultiSelect from '../form/ZMultiSelect';
 import {sizeOptions} from '@/app/dashboard/edit-product/[id]/options';
 import ZInput from '../form/ZInput';
-import {set} from 'zod';
+import {set, size} from 'zod';
 import {generateBarcode} from '@/utils/generateBarcode';
 import {useUpdateProductMutation} from '@/redux/features/product/productApi';
 import {catchAsync} from '@/utils/catchAsync';
@@ -39,7 +41,6 @@ const AddSizeStockModal: React.FC<AddSizeStockModalProps> = ({
 	product,
 	loader,
 }) => {
-	if (!isOpen) return null;
 	const [selectedSizes, setSelectedSizes] = useState<{label: string; value: string}[]>([]);
 	const [defaultValues, setDefaultValues] = useState({});
 	const [updateProduct, {isLoading}] = useUpdateProductMutation();
@@ -54,7 +55,7 @@ const AddSizeStockModal: React.FC<AddSizeStockModalProps> = ({
 			stock_S: getStock('S'),
 			stock_M: getStock('M'),
 			stock_XXL: getStock('XXL'),
-			size: product?.size,
+			size: product?.size || [],
 		});
 
 		setSelectedSizes(
@@ -66,8 +67,6 @@ const AddSizeStockModal: React.FC<AddSizeStockModalProps> = ({
 	}, [product]);
 
 	const handleSubmit = (data: any) => {
-		console.log('Form Data:', data);
-
 		const sizeStocks = selectedSizes.map((size, index) => ({
 			size: size.value,
 			stock: Number(data[`stock_${size.value}`] || 0),
@@ -82,6 +81,7 @@ const AddSizeStockModal: React.FC<AddSizeStockModalProps> = ({
 				data: {
 					stocks_size: sizeStocks,
 					name: product?.name || '',
+					size: data?.size || [],
 				},
 			}).unwrap();
 			if (res?.id) {
@@ -92,6 +92,7 @@ const AddSizeStockModal: React.FC<AddSizeStockModalProps> = ({
 			}
 		});
 	};
+	if (!isOpen) return null;
 	return (
 		<div
 			className={deleteModalStyles.modalOverlay}
@@ -103,52 +104,54 @@ const AddSizeStockModal: React.FC<AddSizeStockModalProps> = ({
 			<div className={deleteModalStyles.modalContent} onClick={(e) => e.stopPropagation()}>
 				<h2 id="add-stock-modal-title">Add Stock for {product?.name}</h2>
 				<div className="input-main-wrapper">
-					<ZForm onSubmit={handleSubmit} defaultValues={defaultValues}>
-						<div className="row">
-							<div className="single-input col-md-12 ">
-								<label htmlFor="productName">Size</label>
-								<ZMultiSelect
-									label="Select Sizes"
-									name="size"
-									options={sizeOptions}
-									onChange={(values: any) => setSelectedSizes(values)}
-								/>
-							</div>
-						</div>
-						{selectedSizes.length > 0 && (
-							<div className="row mt-3">
-								<h5>Enter Stocks Per Size</h5>
-								<div className="row">
-									{selectedSizes.map((size, i) => (
-										<div key={i} className="single-input col-md-6">
-											<label>{size.label} Stock</label>
-											<ZInput
-												name={`stock_${size.value}`}
-												type="number"
-												label={`Stock for size ${size.label}`}
-												height="40px"
-											/>
-										</div>
-									))}
+					{defaultValues && (
+						<ZForm onSubmit={handleSubmit} defaultValues={defaultValues}>
+							<div className="row">
+								<div className="single-input col-md-12 ">
+									<label htmlFor="productName">Size</label>
+									<ZMultiSelect
+										label="Select Sizes"
+										name="size"
+										options={sizeOptions}
+										onChange={(values: any) => setSelectedSizes(values)}
+									/>
 								</div>
 							</div>
-						)}
-						<div className={deleteModalStyles.modalButtons}>
-							<button
-								className={`${deleteModalStyles.btn} ${deleteModalStyles.btnCancel}`}
-								onClick={onClose}
-							>
-								Cancel
-							</button>
-							<button
-								className={`${deleteModalStyles.btn} ${deleteModalStyles.btnDelete}`}
-								onClick={onConfirm}
-								disabled={selectedSizes.length === 0 || isLoading}
-							>
-								{isLoading ? 'Adding...' : 'Add Stock'}
-							</button>
-						</div>
-					</ZForm>
+							{selectedSizes.length > 0 && (
+								<div className="row mt-3">
+									<h5>Enter Stocks Per Size</h5>
+									<div className="row">
+										{selectedSizes.map((size, i) => (
+											<div key={i} className="single-input col-md-6">
+												<label>{size.label} Stock</label>
+												<ZInput
+													name={`stock_${size.value}`}
+													type="number"
+													label={`Stock for size ${size.label}`}
+													height="40px"
+												/>
+											</div>
+										))}
+									</div>
+								</div>
+							)}
+							<div className={deleteModalStyles.modalButtons}>
+								<button
+									className={`${deleteModalStyles.btn} ${deleteModalStyles.btnCancel}`}
+									onClick={onClose}
+								>
+									Cancel
+								</button>
+								<button
+									className={`${deleteModalStyles.btn} ${deleteModalStyles.btnDelete}`}
+									onClick={onConfirm}
+									disabled={selectedSizes.length === 0 || isLoading}
+								>
+									{isLoading ? 'Adding...' : 'Add Stock'}
+								</button>
+							</div>
+						</ZForm>
+					)}
 				</div>
 			</div>
 		</div>
