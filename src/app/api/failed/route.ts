5165c25eebbe45d62719a52b35be9e45b72a4625
Golam Rabbani <golamrabbani9373@gmail.com>
2025-12-17 11:27:24 +0600

    update payment faild route

--- src/app/api/failed/route.ts
+++ src/app/api/failed/route.ts
@@ -3,7 +3,5 @@ import {NextRequest, NextResponse} from 'next/server';
 export async function POST(req: NextRequest) {
 	try {
 		return NextResponse.redirect(new URL('/payment/fail', req.url), 303);
-	} catch (error) {
-		console.log(error);
-	}
+	} catch (error) {}
 }
