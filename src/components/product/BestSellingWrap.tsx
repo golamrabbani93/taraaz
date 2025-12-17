579934ac29a82e1161cedbda233048be9fa1ffef
Golam Rabbani <golamrabbani9373@gmail.com>
2025-12-17 11:45:14 +0600

    make some padding

--- src/components/product/BestSellingWrap.tsx
+++ src/components/product/BestSellingWrap.tsx
@@ -47,7 +47,7 @@ const WeeklyBestSelling = ({
 									</div>
 								</div>
 								<div className="row">
-									<div className="col-lg-12 p-xl-0">
+									<div className={`col-lg-12 p-xl-0 ${head ? '' : 'pt-3'}`}>
 										<div className="row ">
 											{combinedProducts?.map((product: IProduct, index: number) => (
 												<div className="col-xl-3 col-lg-3 col-md-4 col-6 mb-3" key={index}>
